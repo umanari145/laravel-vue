4dfc2c584c057d810b4d35fd9e1314703594e2e8
Norio <umanari145@gmail.com>
2020-02-09 11:23:57 +0900

    APIでcacheを使う

--- app/Http/Controllers/Api/AreaController.php
+++ app/Http/Controllers/Api/AreaController.php
@@ -6,19 +6,31 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\Mjuusho;
 use App\Model\PostCode;
+use Cache;
 
 class AreaController extends Controller
 {
     public function getPref(Request $request)
     {
-        $prefList = Mjuusho::getPrefList();
+        $prefList = Cache::get('prefList');
+        if (!$prefList) {
+            $prefList = Mjuusho::getPrefList();
+            Cache::store('file')->put('prefList', $prefList, 60);
+        }
         return response()->json($prefList, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     public function getCity(Request $request)
     {
         $prefCd = $request->input('pref_cd');
-        $cityList = Mjuusho::getCityList($prefCd);
+        $cacheKey = sprintf('cityList_%s', $prefCd);
+        $cityList = Cache::get($cacheKey);
+
+        if (!$cityList) {
+            $cityList = Mjuusho::getCityList($prefCd);
+            Cache::store('file')->put($cacheKey, $cityList, 60);
+        }
+
         return response()->json($cityList, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
@@ -26,19 +38,28 @@ class AreaController extends Controller
     {
         $prefCd = $request->input('pref_cd');
         $cityCd = $request->input('city_cd');
-        $townList = Mjuusho::getTownList($prefCd, $cityCd);
+        $cacheKey = sprintf('townList_%s_%s', $prefCd, $cityCd);
+        $townList = Cache::get($cacheKey);
+
+        if (!$townList) {
+            $townList = Mjuusho::getTownList($prefCd, $cityCd);
+            Cache::store('file')->put($cacheKey, $townList, 60);
+        }
+
         return response()->json($townList, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     public function getAddress(Request $request)
     {
         $zip = $request->input('zip');
-        $address = PostCode::getAddress($zip);
-        if (!empty($address)) {
+        $cacheKey = sprintf('zip_%s', $zip);
+        $address = Cache::get($cacheKey);
+        if (!$address) {
+            $address = PostCode::getAddress($zip);
             $address->full_address = sprintf('%s%s%s', $address->pref, $address->city, $address->town);
             $address->zip = sprintf('%07d', $address->zip);
+            Cache::store('file')->put($cacheKey, $address, 60);
         }
-
         return response()->json($address, 200, [], JSON_UNESCAPED_UNICODE);
     }
 }
