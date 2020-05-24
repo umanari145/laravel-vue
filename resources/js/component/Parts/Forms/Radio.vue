0983da3fe90b9a95ea294091312b88095bea49b3
Norio <umanari145@gmail.com>
2020-05-24 18:49:13 +0900

    propの型定義エラーを修正

--- resources/js/component/Parts/Forms/Radio.vue
+++ resources/js/component/Parts/Forms/Radio.vue
@@ -20,7 +20,7 @@ export default {
           required:true
         },
         value:{
-            type:String,
+            type:[String,Array],
             required:true
         },
         kv_list:{
