c44ef9c923e538d402054efa5b19d9f738cd6240
umanari145 <umanari145@gmail.com>
2021-05-07 00:04:39 +0900

    １ロットあたりのインサート

--- database/seeds/MjuushoTableSeeder.php
+++ database/seeds/MjuushoTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class MjuushoTableSeeder extends Seeder
 {
@@ -15,12 +16,13 @@ class MjuushoTableSeeder extends Seeder
         $fp = @fopen($filePath, 'r');
 
         $loopIndex = 0;
+        $totalIndex = 0;
+        $addresses = [];
         while ($res = fgetcsv($fp)) {
-            $addresses = [];
-            if ($loopIndex > 1) {
+            if ($loopIndex >= 1) {
                 $addresses[] = [
                     $res[0],
-                    $res[4],
+                    str_replace('-', '', $res[4]),
                     $res[1],
                     $res[7],
                     $res[2],
@@ -33,19 +35,28 @@ class MjuushoTableSeeder extends Seeder
             if ($loopIndex == 10) {
                 $this->addressInsert($addresses);
                 $loopIndex = 1;
+                $addresses = [];
                 continue;
             }
             $loopIndex++;
+            $totalIndex++;
+            echo $totalIndex. "\n";
+            if ($totalIndex === 100) {
+                break;
+            }
         }
     }
 
     private function addressInsert(array $addresses)
     {
-        $sql = "INSERT INTO m_juusho (address_cd, zip, m_prefectures_id, ken, shikuchouson_cd, shikuchouson, oaza_cd, oaza, aza) VALUES ";
+        $sql = "INSERT INTO m_juusho (address_cd, zip, m_prefectures_id, ken, shikuchouson_cd, shikuchouson, ooaza_cd, ooaza, aza) VALUES ";
         $prepareSQlArr = array_fill(0, count($addresses[0]), "?");
         $prepareSQl = implode(',', $prepareSQlArr);
         $prepareSQl2 = sprintf('(%s)', $prepareSQl);
         $allPrepareSQlArr = array_fill(0, count($addresses), $prepareSQl2);
-//        $allPrepareSQl = implode("");
+        $sql .= implode(',', $allPrepareSQlArr);
+        $bindValue = collect($addresses)->flatten()->toArray();
+        DB::insert($sql, $bindValue);
+        exit('aaa');
     }
 }
